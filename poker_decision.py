44dd8549e012430d7080c7c314fb2347d9465d3b
Martin Peron <martin.peron@blackmoth.com>
2021-02-24 17:30:35 +1000

    calculated and plotted P1's EV against bet size and bluff percentage

--- poker_decision.py
+++ poker_decision.py
@@ -5,19 +5,34 @@ import numpy as np
 
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-# Make data.
-bet = np.arange(0, 5, 0.25)
-bluff = np.arange(0, 1, 0.1)
+# bet/bluff grid
+coarseness = 0.1
+bet = np.arange(0, 10, 0.25 * coarseness)  # we assume a pot size of 1
+bluff = np.arange(0, 1, 0.001 * coarseness)
 bet, bluff = np.meshgrid(bet, bluff)
-EV_P2 = np.sqrt(bet**2 + bluff**2)
-Z = np.sin(R)
+
+# P2's decisions
+EV_P2_folds_when_P1_bets = 0
+EV_P2_calls_when_P1_bets = (bluff + (bluff - 1) * bet) / (1 + bluff)
+
+# P1's decisions
+Prob_P1_bets = 1/2 + 1/2 * bluff  # always bets with good hands, sometimes bluffs with bad hands
+EV_P1_if_P1_doesnt_bet = 0  # no bet => bad hand => always loses
+EV_P1_if_P1_bets_and_P2_folds = 1  # P1 always wins except when she doesn't bluff
+EV_P1_if_P1_bets_and_P2_calls = (1 + bet - bluff * bet) / (1 + bluff)
+EV_P1_if_P1_bets = np.where(EV_P2_calls_when_P1_bets > EV_P2_folds_when_P1_bets,
+                            EV_P1_if_P1_bets_and_P2_calls, EV_P1_if_P1_bets_and_P2_folds)
+EV_P1 = Prob_P1_bets * EV_P1_if_P1_bets + (1 - Prob_P1_bets) * EV_P1_if_P1_doesnt_bet
 
 # Plot the surface.
-surf = ax.plot_surface(bet, bluff, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+surf = ax.plot_surface(bet, bluff, EV_P1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=True)
 
 # Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
+ax.set_zlim(0, 1)
+ax.set_xlabel("Bet size")
+ax.set_ylabel("Bluff %")
+ax.set_zlabel("EV")
 ax.zaxis.set_major_locator(LinearLocator(10))
 # A StrMethodFormatter is used automatically
 ax.zaxis.set_major_formatter('{x:.02f}')
